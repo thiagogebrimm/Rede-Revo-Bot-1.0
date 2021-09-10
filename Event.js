3454fd1e0216800ddfe741010b24bcf73b0a3c6f
Thiago Gebrim <75040244+thiagogebrimm@users.noreply.github.com>
2021-09-10 15:16:08 -0300

    Adição da pasta func

---
+++ Event.js
@@ -0,0 +1,13 @@
+const { readdirSync } = require('fs');
+const { join } = require('path');
+const filePath = join(__dirname, "..", "eventos");
+
+module.exports.run = (bot) => {
+    for (const eventFile of readdirSync(filePath)) {
+        const event = require(`${filePath}/${eventFile}`);
+        const eventName = eventFile.split(".").shift();
+        bot.on(eventName, event.bind(null, bot));
+    }
+    
+    console.log(`Carreguei ${readdirSync(filePath).length} eventos!`)
+}
