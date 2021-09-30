11c2fb80b43fcf613ef5b51cf51ff4c0f036164e
thiag <thiagogebrim@hotmail.com>
2021-09-30 19:24:59 -0300

    Melhoria no clear (erro conhecido: o bot fica enviando o comando mesmo depois de enviar)

--- src/scmds/Admin/clear.js
+++ src/scmds/Admin/clear.js
@@ -28,17 +28,21 @@ module.exports = {
             }).then(valor => valor.toJSON())
 
             
-            
             interaction.channel.bulkDelete(clean) // tentaremos deletar a quantia que o membro pediu
             // enviando uma embed
+            let logfeito = new Discord.MessageEmbed()
+            .setDescription(`O staff ${interaction.member.toString()} teve êxito em apagar **** mensagens no canal ${interaction.channel.toString()}.`)
+            .setColor(`GREEN`)
             let embed = new Discord.MessageEmbed()
-    
-            .setTitle(`<:lixo:860124969223716865> LIMPEZA!`)
-            .setDescription(`<:aviso:854929386394615848> Limpei um total de \`${valor.length}\` mensagens.`)
+            .setTitle(`<:PepoLixo_Revo:893232516088070175> LIMPO!`)
+            .setDescription(`<:Alerta_Revo:870516091330388058> Limpei um total de \`${valor.length}\` mensagens.`)
             .setFooter(`Autor: ${interaction.user.username}`)
     
             interaction.channel.send({
                 embeds: [embed]
+            })
+            interaction.guild.channels.cache.find(x => x.id === '793599388420800543').send({
+                embeds: [logfeito]
             })
 
     } catch(error) {
