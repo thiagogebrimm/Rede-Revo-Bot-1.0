c9d8f1b3ba3b1023cc2b8ce8733d427b2ad87ae7
thiag <thiagogebrim@hotmail.com>
2021-10-03 17:23:24 -0300

    Correção do comando IP

--- src/scmds/Minecraft/ip.js
+++ src/scmds/Minecraft/ip.js
@@ -1,19 +1,20 @@
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-    name: 'ip',
-    aliases: [''],
-    category : 'Minecraft',
-    description: 'Veja o nosso IP!',
-    usage: '',
+  name: 'ip',
+  aliases: [''],
+  category: 'Minecraft',
+  description: 'Veja o nosso IP!',
+  usage: '',
 
-      run: async(client, interaction) => {
-        let embed = new MessageEmbed() 
-        .setColor(`#0099ff`) 
-        .setTitle(`Atualmente nosso ip é`) 
-        .setDescription('<a:DirtyPulando_Revo:852710345012543490> **IP:** rederevo.com\n\n**<a:DirtyPulando_Revo:852710345012543490> Bedrock:** jogar.rederevo.com:19132')
-        .setFooter('Rede Revo', 'https://i.imgur.com/dMswI7f.png');
-     await interaction.editReply({
-       embeds: [embed]
-     }); 
-      }
-    }
+  run: async (client, interaction) => {
+    let embed = new MessageEmbed()
+      .setColor(`#0099ff`)
+      .setTitle(`Atualmente nosso ip é`)
+      .setDescription('<a:DirtyPulando_Revo:852710345012543490> **IP:** rederevo.com\n\n**<a:DirtyPulando_Revo:852710345012543490> Bedrock:** jogar.rederevo.com:19132')
+      .setFooter('Rede Revo', 'https://i.imgur.com/dMswI7f.png');
+    await interaction.editReply({
+      embeds: [embed]
+    });
+  }
+}
