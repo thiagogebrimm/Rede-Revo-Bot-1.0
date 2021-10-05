a27411eb4e235a67bcb8a78585762dc3dfea2fc2
thiag <thiagogebrim@hotmail.com>
2021-10-05 17:32:48 -0300

    Correção no sistema de log (messageUpdate)

--- src/eventos/messageUpdate.js
+++ src/eventos/messageUpdate.js
@@ -1,18 +1,21 @@
 const { MessageEmbed } = require("discord.js")
 
 module.exports = (bot, oldMessage, newMessage) => {
-    if(oldMessage.guild == null) return;
 
     let canal = oldMessage.guild.channels.cache.find(x => x.id === '793599388420800543')
         if(newMessage.content != oldMessage.content){
             if(!canal) return;
             canal.send({embeds: [new MessageEmbed()
-                .setTitle(`MENSAGEM EDITADA`)
-                .setColor(`36393e`) 
-                .setDescription(`[Clique aqui para ser redirecionado a mensagem.](https://discordapp.com/channels/${oldMessage.guild.id}/${oldMessage.channel.id}/${oldMessage.id})`)
-                .addField(`📝 Mensagem antiga`, `${oldMessage.content}.`, true)
-                .addField(`✏️ Nova mensagem`, `${newMessage.content}.`, true)
-                .setFooter(`Autor da mensagem: ${oldMessage.author.tag}.`)
+                .setTitle(`MENSAGEM EDITADA EM #${newMessage.channel.name}`)
+                .setColor(`4682B4`) 
+                .setDescription(`[Clique aqui para ser redirecionado a mensagem.](https://discordapp.com/channels/${oldMessage.guild.id}/${oldMessage.channel.id}/${oldMessage.id})
+                
+                📝 **Mensagem antiga:** ${oldMessage.content}
+
+                ✏️ **Nova mensagem:**  ${newMessage.content}
+                `)
+                .setTimestamp(newMessage.createdAt)
+                .setFooter(`Autor da mensagem: ${oldMessage.author.tag}`, oldMessage.author.avatarURL({ dynamic: true }))
             ]})
         }
         return;
