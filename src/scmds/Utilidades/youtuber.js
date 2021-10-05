b0e54c4a2aa614c6f4302de07ac592bc0a0842ec
thiag <thiagogebrim@hotmail.com>
2021-10-05 01:03:16 -0300

    Correção no embed do /yt

--- src/scmds/Utilidades/youtuber.js
+++ src/scmds/Utilidades/youtuber.js
@@ -11,7 +11,36 @@ module.exports = {
     let embed = new MessageEmbed()
       .setColor(`#0099ff`)
       .setTitle(`Segue abaixo os requisitos para as tags YT e YT+`)
-      .setDescription('**YT:**\n**250 - 1000** inscritos\n**1** vídeo por semana\n**25%** de visualizações em relação aos inscritos\n\n**YT+:**\n**1000 - 5000** inscritos\n**1** vídeo por semana\n**25%** de visualizações em relação aos inscritos\n\n\n<:Gold_Revo:854554725772296192> **Sistema de recompensas**\n\n**YT:**\n- Aviso de entrada destacada no lobby\n- Possibilidade de definir **10** homes\n- **300** cash por vídeo semanal caso a parceria seja cumprida corretamente\n- Comando de divulgação exclusivo dentro do servidor\n- Acesso ao #🎬・divulgação\n\n**YT+:**\n- Aviso de entrada destacada no lobby\n- Possibilidade de definir **10** homes\n- **500** cash por vídeo semanal caso a parceria seja cumprida corretamente\n- Comando de divulgação exclusivo dentro do servidor\n- Acesso ao #🎬・divulgação\n\nCumpre os requisitos acima? Favor abrir um #📩・ticket pra autenticar a parceria.\n\n**OBS:** Lembrando que ao você se tornar Youtuber e Youtuber+ suas ações representam diretamente o servidor. Então sempre aja de acordo com as regras.')
+      .setDescription(`**YT:**
+      **250 - 1000** inscritos
+      **1** vídeo por semana
+      **25%** de visualizações em relação aos inscritos
+      
+      **YT+:**
+      **1000 - 5000** inscritos
+      **1** vídeo por semana
+      **25%** de visualizações em relação aos inscritos
+      
+      
+      <:Gold_Revo:854554725772296192> **Sistema de recompensas**
+      
+      **YT:**
+      - Aviso de entrada destacada no lobby
+      - Possibilidade de definir **10** homes
+      - **300** cash por vídeo semanal caso a parceria seja cumprida corretamente
+      - Comando de divulgação exclusivo dentro do servidor
+      - Acesso ao <#795426717132390441>
+      
+      **YT+:**
+      - Aviso de entrada destacada no lobby
+      - Possibilidade de definir **10** homes
+      - **500** cash por vídeo semanal caso a parceria seja cumprida corretamente
+      - Comando de divulgação exclusivo dentro do servidor
+      - Acesso ao <#795426717132390441>
+      
+      Cumpre os requisitos acima? Favor abrir um #📩・ticket pra autenticar a parceria.
+      
+      **OBS:** Lembrando que ao você se tornar Youtuber e Youtuber+ suas ações representam diretamente o servidor. Então sempre aja de acordo com as regras.`)
       .setFooter('Rede Revo', 'https://i.imgur.com/dMswI7f.png');
     interaction.editReply({
       embeds: [embed]
