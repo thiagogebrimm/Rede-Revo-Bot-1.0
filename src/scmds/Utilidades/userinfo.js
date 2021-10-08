0f9ad890b04a29eb93c730ef2778e0ae66d1f7b5
thiag <thiagogebrim@hotmail.com>
2021-10-08 16:08:17 -0300

    Comando userinfo on e corrigido

--- src/scmds/Utilidades/userinfo.js
+++ src/scmds/Utilidades/userinfo.js
@@ -1,81 +1,39 @@
 const moment = require('moment')
-
-const { Client, CommandInteraction, MessageEmbed } = require('discord.js');
+moment.locale('pt-br');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-    name: 'userinfo',
-    aliases: [''],
-    category: 'Utilidades',
-    description: 'Veja informações de um usuario',
-    usage: '',
-    options: [
-      {
-        name: 'user',
-        type: 'USER',
-        required: false,
-        description:"Escollha um usuario"
-      }
-    ],
-      run: async(client, interaction, args) => {
-        const member = interaction.options.getMember("user") || interaction.member
-      
-        const activities = [];
-        for (const activity of member.user.presence.activities.values()) {
-          switch (activity.type) {
-            case 'PLAYING':
-              activities.push(`Jogando **${activity.name}**`);
-              break;
-            case 'LISTENING':
-              if (member.user.bot) activities.push(`Escutando **${activity.name}**`);
-              else activities.push(`Escutando **${activity.details}** de **${activity.state}**`);
-              break;
-            case 'WATCHING':
-              activities.push(`Assistindo **${activity.name}**`);
-              break;
-            case 'STREAMING':
-              activities.push(`Transmitindo **${activity.name}**`);
-              break;
-            case 'CUSTOM_STATUS':
-              customStatus = activity.state;
-              break;
-          }}
-    
-    
-          let obj = { false: 'Não', true: 'Sim' }
-    
-    
-      const infoEmbed = new MessageEmbed()
-      .setColor("RANDOM")
-      .setTitle(`Informações sobre ${member.user.username}`)
-      .setThumbnail(member.user.avatarURL({dynamic: true}))
-      .setFooter('Solicitado')
-      .setTimestamp()
-      .addFields(
-          { 
-              name: "Informações do Usuario",
-              value: "```Username:"+member.user.username+"\nDiscriminator: #"+member.user.discriminator+"\nTag: "+member.user.tag+"\nNickname: "+member.displayName+"\nÉ um bot?: "+obj[member.user.bot]+"\nID: "+member.user.id+" ```",
-              inline: true
-          },
-          {
-              name: `Status`,
-              value: "```"+member.user.presence.status+"\n"+activities+"```",
-              inline: false
-          },
-          {
-              name: `Datas`,
-              value: "```Entrou aqui em: "+moment(member.joinedTimestamp).format('LLL')+"\n\nConta criada em: "+moment(member.user.createdTimestamp).format('LLL')+"```",
-              inline: true
-          },
-          {
-            name: `Cargos`,
-            value: ""+member.roles.cache.map(r => r).join(' | ')+"",
-            inline: true
-          },
-        
-    
-      )
-      interaction.editReply({
-        embeds: [infoEmbed]
-      })
-      }
+  name: 'userinfo',
+  aliases: [''],
+  category: 'Utilidades',
+  description: 'Veja informações de um membro',
+  usage: '',
+  options: [
+    {
+      name: 'membro',
+      type: 'USER',
+      required: false,
+      description: "Escollha um membro"
     }
+  ],
+  run: async (bot, interaction) => {
+    const member = interaction.options.getMember("membro") || interaction.member
+
+    let obj = { false: 'Não', true: 'Sim' }
+
+    const infoEmbed = new MessageEmbed()
+      .setColor("RANDOM")
+      .setThumbnail(member.user.avatarURL({ dynamic: true }))
+      .addField(`${member.user.tag}`, `${member.user}`, true)
+      .addField("ID do Discord:", `${member.user.id}`, true)
+      .addField("Nick:", `${member.user.username}`, true)
+      .addField("É Bot:", `${obj[member.user.bot]}`, true)
+      .addField("Entrou no servidor:", `${moment(member.joinedTimestamp).utc(-3).format("DD [de] MMMM[,] YYYY [ás] HH:mm")} (${moment(member.joinedTimestamp).utc(-3).fromNow()})`, true)
+      .addField("Conta criada:", `${moment(member.user.createdAt).utc(-3).format("DD [de] MMMM[,] YYYY [ás] HH:mm")} (${moment(member.user.createdAt).utc(-3).fromNow()})`, true)
+      .setFooter(`Respondendo para: ${interaction.user.username}#${interaction.user.discriminator}`)
+
+    interaction.editReply({
+      embeds: [infoEmbed]
+    })
+  }
+}
