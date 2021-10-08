a242624b91237fb952b1b349c891443f15ac2f1c
thiag <thiagogebrim@hotmail.com>
2021-10-08 18:11:13 -0300

    Corrigido erro no userinfo

--- src/scmds/Utilidades/userinfo.js
+++ src/scmds/Utilidades/userinfo.js
@@ -1,6 +1,6 @@
 const moment = require('moment')
 moment.locale('pt-br');
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, GuildMember } = require('discord.js');
 
 module.exports = {
   name: 'userinfo',
@@ -26,7 +26,7 @@ module.exports = {
       .setThumbnail(member.user.avatarURL({ dynamic: true }))
       .addField(`${member.user.tag}`, `${member.user}`, true)
       .addField("ID do Discord:", `${member.user.id}`, true)
-      .addField("Nick:", `${member.user.username}`, true)
+      .addField("Nick:", `${member.displayName}`, true)
       .addField("É Bot:", `${obj[member.user.bot]}`, true)
       .addField("Entrou no servidor:", `${moment(member.joinedTimestamp).utc(-3).format("DD [de] MMMM[,] YYYY [ás] HH:mm")} (${moment(member.joinedTimestamp).utc(-3).fromNow()})`, true)
       .addField("Conta criada:", `${moment(member.user.createdAt).utc(-3).format("DD [de] MMMM[,] YYYY [ás] HH:mm")} (${moment(member.user.createdAt).utc(-3).fromNow()})`, true)
