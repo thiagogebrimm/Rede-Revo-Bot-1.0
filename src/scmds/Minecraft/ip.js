d9b9b88d4807ed9db63449e850e232baa3aa5da4
Thiago Gebrim <thiagogebrim@hotmail.com>
2021-12-02 20:56:59 -0300

    Pequenas alterações

--- src/scmds/Minecraft/ip.js
+++ src/scmds/Minecraft/ip.js
@@ -11,8 +11,10 @@ module.exports = {
     let embed = new MessageEmbed()
       .setColor(`#0099ff`)
       .setTitle(`Atualmente nosso ip é`)
-      .setDescription('<a:DirtyPulando_Revo:852710345012543490> **IP:** rederevo.com\n\n**<a:DirtyPulando_Revo:852710345012543490> Bedrock:** jogar.rederevo.com:19132')
-      .setFooter('Rede Revo', 'https://i.imgur.com/dMswI7f.png');
+      .setDescription(`<a:DirtyPulando_Revo:852710345012543490> **Java:** jogar.rederevo.com
+      
+      **<a:DirtyPulando_Revo:852710345012543490> Bedrock:** jogar.rederevo.com:19132`)
+      .setFooter('Equipe Rede Revo', interaction.guild.iconURL({ dynamic: true }));
     await interaction.editReply({
       embeds: [embed]
     });
