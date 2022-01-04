0ba0bdacae004df5e6dffec93bc48cf95cfc839d
Thiago Gebrim <thiagogebrim@hotmail.com>
2022-01-04 15:18:11 -0300

    Alinhando embed

--- src/scmds/Admin/embedwiki.js
+++ src/scmds/Admin/embedwiki.js
@@ -14,10 +14,10 @@ module.exports = {
     let embedRegras = new MessageEmbed()
       .setTitle(`<:Revo:857398325195833344> Perguntas Frequentes <:Revo:857398325195833344>`)
       .setDescription(`
-      **<:Estrela_Revo:917616701774909470> | [Como funciona o Cash?](https://wiki.rederevo.com/sistemas/sistema-de-cash)**
-      **<:Espadas_Revo:890297997471154186>  |** [Qual é a agenda de Eventos?](https://wiki.rederevo.com/eventos/eventos-do-servidor/agenda-de-eventos)
-      **<:PepoSeiLa_Revo:846192575290408990>   |** [Quanto tempo eu posso ficar offline até perder meu terreno?](https://wiki.rederevo.com/sistemas/sistema-de-protecao/protecao-abandonada)
-        `)
+**<:Estrela_Revo:917616701774909470> | [Como funciona o Cash?](https://wiki.rederevo.com/sistemas/sistema-de-cash)**
+**<:Espadas_Revo:890297997471154186> |** [Qual é a agenda de Eventos?](https://wiki.rederevo.com/eventos/eventos-do-servidor/agenda-de-eventos)
+**<:PepoSeiLa_Revo:846192575290408990> |** [Quanto tempo eu posso ficar offline até perder meu terreno?](https://wiki.rederevo.com/sistemas/sistema-de-protecao/protecao-abandonada)
+      `)
       .setColor('#836FFF')
 
     interaction.deleteReply()
