0727b996cbf2b77bdfd348c8346a355b0f8697de
Thiago Gebrim <75040244+thiagogebrimm@users.noreply.github.com>
2022-03-16 02:02:19 -0300

    Mutar agr coloca de castigo

--- src/scmds/Admin/mute.js
+++ src/scmds/Admin/mute.js
@@ -5,7 +5,7 @@ module.exports = {
     name: 'mute',
     aliases: ['mutar'],
     category: 'Admin',
-    description: 'Muta um membro',
+    description: 'Coloca um membro de castigo',
     usage: '',
     options: [
         {
@@ -29,57 +29,43 @@ module.exports = {
     ],
 
     run: async (client, interaction, args) => {
-        if (!interaction.member.permissions.has(['MANAGE_MESSAGES'])) return interaction.editReply("Sem permissão para executar esse comando!");
-        const member = interaction.options.getMember("membro")
-        if (!member) {
-            interaction.editReply({
-                embeds: [new MessageEmbed()
-                    .setDescription(`Você deve mencionar um membro válido.`)
-                    .setColor(`RED`)]
-            })
-        } else {
-            if (member.roles.highest.position >= interaction.member.roles.highest.position && interaction.user.id !== interaction.guild.owner.id) return;
+        const user = interaction.options.getUser("membro");
+        const tempo = interaction.options.getString("tempo");
+        const reason = interaction.options.getString("motivo");
+        const member = interaction.guild.members.cache.get(user.id);
 
-            let time = interaction.options.getString("tempo");
-            if (!time || isNaN(ms(args[1]))) return interaction.editReply({
-                embeds: [new MessageEmbed()
-                    .setDescription(`Você deve dizer um tempo válido (s, m, d ou h)`)
-                    .setColor(`36393e`)]
-            })
+        const timeInMs = ms(tempo)
+        if (!timeInMs) return interaction.followUp({
+            embeds: [new MessageEmbed()
+                .setDescription(`Você deve dizer um tempo válido (s, m, d ou h)`)
+                .setColor(`36393e`)]
+        })
 
-            interaction.editReply({
-                embeds: [new MessageEmbed()
-                    .setColor(`RED`)
-                    .setDescription(`${member.displayName.toString()} foi punido!`)]
-            });
+        member.timeout(timeInMs, reason)
+        interaction.followUp({
+            embeds: [new MessageEmbed()
+                .setColor(`RED`)
+                .setDescription(`${member.displayName.toString()} foi punido!`)]
+        });
 
-            let reason = interaction.options.getString("motivo");
-            if (!reason) reason === 'Nenhum motivo aparente.'
-            interaction.guild.channels.cache.find(x => x.id === '849452824970264626').send({
-                embeds: [new MessageEmbed()
-                    .setTitle(`<:Press_F_Revo:850543446003286017> Nova Punição no Discord`)
-                    .setDescription(`
+        interaction.guild.channels.cache.find(x => x.id === '849452824970264626').send({
+            embeds: [new MessageEmbed()
+                .setTitle(`<:Press_F_Revo:850543446003286017> Nova Punição no Discord`)
+                .setDescription(`
 \`${member.displayName.toString()}\` foi silenciado(a) por ${interaction.member.toString()}.
 Motivo: \`${reason}\`
-Duração: \`${time}\``)
-                    .setColor(`RED`)]
-            });
+Duração: \`${tempo}\``)
+                .setColor(`RED`)]
+        });
 
-            member.roles.add('847830245851660290');
-
-            member.send({
-                embeds: [new MessageEmbed()
-                    .setTitle(`<:Press_F_Revo:850543446003286017>Você não seguiu as regras e foi punido`)
-                    .setDescription(`
+        member.send({
+            embeds: [new MessageEmbed()
+                .setTitle(`<:Press_F_Revo:850543446003286017>Você não seguiu as regras e foi punido`)
+                .setDescription(`
 Você foi silenciado(a) por ${interaction.member.toString()}.
 Motivo: \`${reason}\`
-Duração: \`${time}\``)
-                    .setColor(`RED`)]
-            }).catch(a => { return message.channel.send(`Impossivel mandar mensagens na DM deste usuario para notifica-lo!`) });
-
-            setTimeout(async () => {
-                member.roles.remove('847830245851660290');
-            }, ms(time));
-        }
+Duração: \`${tempo}\``)
+                .setColor(`RED`)]
+        }).catch(a => { return message.channel.send(`Impossivel mandar mensagens na DM deste usuario para notifica-lo!`) });
     }
 }
