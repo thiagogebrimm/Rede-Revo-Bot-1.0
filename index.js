b9ac2190c0b134d949ed0b9234145cdb2d58eb17
Thiago Gebrim <75040244+thiagogebrimm@users.noreply.github.com>
2022-03-17 11:43:36 -0300

    Melhorias no sistema de starboard

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const { token } = require('./token.json');
 const { Client, Collection } = require('discord.js');
-
+const simpl = require("simpl.db")
+const stardb = simpl({
+    autoSave: true,
+    dataFile: './starboard.json'
+})
 const bot = new Client({
     intents: ["GUILD_VOICE_STATES", "GUILDS", "GUILD_BANS", "GUILD_PRESENCES", "GUILD_EMOJIS_AND_STICKERS", "GUILD_INTEGRATIONS", "GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "DIRECT_MESSAGE_REACTIONS", "DIRECT_MESSAGES"], ws: {
     }
 });
+
+bot.stardb = stardb
 
 bot.slashCommands = new Collection();
 bot.limits = new Map();
--- src/eventos/messageReactionAdd.js
+++ src/eventos/messageReactionAdd.js
@@ -1,9 +1,16 @@
-const { Client, MessageEmbed } = require("discord.js");
+const { Client, User, MessageReaction, MessageEmbed } = require("discord.js");
 const config = require("../../starboard")
+/**
+ *
+ * @param {Client} client
+ * @param {MessageReaction} reaction
+ * @param {User} user
+ */
 
 module.exports = async (client, reaction, user) => {
     if(reaction.emoji.name !== config.emoji) return;
     if(reaction.users.cache.filter((f) => !f.bot).size >= config.startcount){
+        if(client.stardb.get(reaction.message.id)) return;
         let embed = new MessageEmbed()
         .setColor("YELLOW")
         .setTitle("⭐ Starboard")
@@ -18,5 +25,11 @@ Mensagem enviada por: **${reaction.message.author.username}**
         let starboard = client.channels.cache.find(x => x.id === config.channel)
         if(!starboard) return;
         starboard.send({embeds: [embed]})
+        client.stardb.set(reaction.message.id, {
+            id: reaction.message.id,
+            channel: reaction.message.channel.id,
+            guild: reaction.message.guild.id,
+            author: reaction.message.author.id
+        })
     }
 };
