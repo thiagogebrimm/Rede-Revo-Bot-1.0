3eec37e0c095ac6861ba3533fb654d11c9358cd3
Thiago Gebrim <75040244+thiagogebrimm@users.noreply.github.com>
2022-03-19 00:04:58 -0300

    Correções nos eventos

--- src/eventos/guildMemberAdd.js
+++ src/eventos/guildMemberAdd.js
@@ -7,7 +7,7 @@ module.exports = (bot, member) => {
 
     canallog.send({
         embeds: [new MessageEmbed()
-            .setAuthor({ text: `Nova entrada no Discord`, string: member.user.displayAvatarURL({ dynamic: true }) })
+            .setAuthor({ name: `Nova entrada no Discord`, string: member.user.displayAvatarURL({ dynamic: true }) })
             .setDescription(`${member.user} **entrou em nosso discord, sua conta foi criada em:** ${moment(member.user.createdTimestamp).utc(-3).format('DD[/]MM[/]YYYY [ás] HH:mm')} (${moment(member.user.createdTimestamp).utc(-3).fromNow()})`)
             .setTimestamp(member.joinedTimestamp)
             .setFooter({ text: `${member.user.tag}` })
--- src/eventos/guildMemberRemove.js
+++ src/eventos/guildMemberRemove.js
@@ -7,7 +7,7 @@ module.exports = (bot, member) => {
 
     canallog.send({
         embeds: [new MessageEmbed()
-            .setAuthor({ text: `Nova saída no Discord`, string: member.user.displayAvatarURL({ dynamic: true }) })
+            .setAuthor({ name: `Nova saída no Discord`, string: member.user.displayAvatarURL({ dynamic: true }) })
             .setDescription(`${member.user} **entrou em nosso discord em:** ${moment(member.joinedTimestamp).utc(-3).format('DD[/]MM[/]YYYY [ás] HH:mm')} (${moment(member.joinedTimestampt).utc(-3).fromNow()})`)
             .setTimestamp(member.removedTimestamp)
             .setFooter({ text: `${member.user.tag}` })
