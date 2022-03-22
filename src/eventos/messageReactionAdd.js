dedbaa200a378a80fc3d63338cdc9a44604f1599
Thiago Gebrim <75040244+thiagogebrimm@users.noreply.github.com>
2022-03-22 19:43:59 -0300

    nick nos stars

--- src/eventos/messageReactionAdd.js
+++ src/eventos/messageReactionAdd.js
@@ -2,23 +2,26 @@ const { MessageEmbed } = require("discord.js");
 const config = require("../../starboard")
 
 module.exports = async (client, reaction, user) => {
-    if(reaction.emoji.name !== config.emoji) return;
-    if(reaction.users.cache.filter((f) => !f.bot).size >= config.startcount){
-        if(client.stardb.get(reaction.message.id)) return;
+
+    const nick = reaction.message.guild.members.cache.get(reaction.message.author.id)
+
+    if (reaction.emoji.name !== config.emoji) return;
+    if (reaction.users.cache.filter((f) => !f.bot).size >= config.startcount) {
+        if (client.stardb.get(reaction.message.id)) return;
         let embed = new MessageEmbed()
-        .setColor("YELLOW")
-        .setTitle(`⭐ ${reaction.message.author.username}`)
-        .addField("Ir para mensagem", `**[Clique aqui](https://discordapp.com/channels/${reaction.message.guild.id}/${reaction.message.channel.id}/${reaction.message.id})**`)
-        .setTimestamp(reaction.message.createdAt)
-        if(reaction.message.content.length >= 1) embed.setDescription(`${reaction.message.content}`)
-        if(reaction.message.attachments.size > 0){
+            .setColor("YELLOW")
+            .setTitle(`⭐ ${nick.displayName}`)
+            .addField("Ir para mensagem", `**[Clique aqui](https://discordapp.com/channels/${reaction.message.guild.id}/${reaction.message.channel.id}/${reaction.message.id})**`)
+            .setTimestamp(reaction.message.createdAt)
+        if (reaction.message.content.length >= 1) embed.setDescription(`${reaction.message.content}`)
+        if (reaction.message.attachments.size > 0) {
             let attachments = reaction.message.attachments.map((a) => `🖇️ | [${a.name}](${a.url})`).join("\n")
             embed.addField("Arquivos", attachments)
             embed.setImage(reaction.message.attachments.first().url)
         }
         let starboard = client.channels.cache.find(x => x.id === config.channel)
-        if(!starboard) return;
-        starboard.send({content: `**${reaction.users.cache.filter((f) => !f.bot).size} -** <#${reaction.message.channel.id}>`, embeds: [embed]})
+        if (!starboard) return;
+        starboard.send({ content: `**${reaction.users.cache.filter((f) => !f.bot).size} -** <#${reaction.message.channel.id}>`, embeds: [embed] })
         client.stardb.set(reaction.message.id, {
             id: reaction.message.id,
             channel: reaction.message.channel.id,
